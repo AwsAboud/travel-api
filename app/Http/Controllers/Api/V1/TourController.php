dee0ff1e383a9eb619c557c448aaf7b603218105
AwsAboud <aws.amin.aboud@gmail.com>
2024-02-28 11:11:06 +0300

    add validation to tour controller to ensure user input is valid

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ToursListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
 use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel, Request $request){
-        // dont forget validation
+    public function index(Travel $travel, ToursListRequest $request){
         $tours =  $travel->toures()
             ->when($request->priceFrom, function($query) use($request){
                 $query->where('price', '>=', $request->priceFrom * 100);
